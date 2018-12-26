113d46fbaa4bd35e80e1b665fb7ef64b0c363e2e
nfleury <nicholasfleury@gmail.com>
2018-12-26 23:28:23 +0400

    use wss, temporary trxplorer address

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 const conf = {
     "production": {
         api:"http://151.80.41.202:7070",
-        ws:"ws://151.80.41.202:7272"
+        ws:"wss://wss.trxplorer.io"
     },
     "development": {
         api:"http://localhost:7171",
